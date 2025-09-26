68aa8b578c66132b945054d2716150c97c4b71e8
IzpoDev <ponce.1714.frp@gmail.com>
2025-09-26 00:13:58 -0500

    Soluciion del puerto

--- app.py
+++ app.py
@@ -263,5 +263,5 @@ def register_socketio_events(socketio):
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT'))
     socketio.run(app, host='0.0.0.0', port=port, debug=False)
